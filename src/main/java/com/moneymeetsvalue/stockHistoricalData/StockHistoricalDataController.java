0ff0586b2bca417edc1e4a48455b2843f6508c7c
unknown <orelhochenboym@gmail.com>
2022-09-10 13:12:30 +0300

    initial commit

---
+++ src/main/java/com/moneymeetsvalue/stockHistoricalData/StockHistoricalDataController.java
@@ -0,0 +1,10 @@
+package com.moneymeetsvalue.stockHistoricalData;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class StockHistoricalDataController {
+
+}
