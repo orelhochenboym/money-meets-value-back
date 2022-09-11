182a3832f25454f79d59c1fde0a0cbe887864058
unknown <orelhochenboym@gmail.com>
2022-09-12 00:37:10 +0300

    working search, fix data, missing calculate

--- src/main/java/com/moneymeetsvalue/MoneyMeetsValueApplication.java
+++ src/main/java/com/moneymeetsvalue/MoneyMeetsValueApplication.java
@@ -6,7 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoneyMeetsValueApplication {
 
+	/**
+	 * main function that's apart of Spring Boot.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		/**
+		 * Make an indented JSON output. It makes the JSON output be considered as
+		 * "pretty printed".
+		 */
+		System.setProperty("spring.jackson.serialization.INDENT_OUTPUT", "true");
 		SpringApplication.run(MoneyMeetsValueApplication.class, args);
 	}
 
